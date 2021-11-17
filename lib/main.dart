30d7aea9ab92e7cab2d8e35fb43d39c264a134b7
Ítalo Covas <italocovas@gmail.com>
2021-11-17 15:57:09 -0300

    Alterações na página de Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-     home: HomePage(),
-    ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        dividerColor: Colors.transparent,
+      ),
+    );
+  }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-  String dropdownValue = 'português(Brasil)';
+  String dropdownValue = 'português (Brasil)';
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +36,102 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
+        elevation: 0,
         backgroundColor: Colors.white,
         title: DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+          value: dropdownValue,
+          icon: const Icon(Icons.keyboard_arrow_down, color: Colors.blueGrey),
+          iconSize: 24,
+          elevation: 14,
+          style: const TextStyle(color: Colors.blueGrey),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+          items: <String>[
+            'português (Brasil)',
+            'inglês (US)',
+            'español (España)'
+          ].map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['português(Brasil)', 'English(US)', 'Español(España)']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(25.0, 130.0, 25.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image(
+              image: AssetImage(
+                'assets/images/logo.png',
+              ),
+              width: 70.0,
+              height: 70.0,
+            ),
+            Divider(height: 25.0),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: 'Número de telefone, email ou nome de usuário',
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, digite uma das opções para fazer login!';
+                }
+                return null;
+              },
+            ),
+            Divider(height: 25.0),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Senha',
+                suffixIcon: Icon(Icons.remove_red_eye_outlined),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Por favor, digite sua senha para entrar!';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 25.0, bottom: 25.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                  color: Colors.blue[300],
+                ),
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(color: Colors.grey, fontSize: 16.0),
+                children: <TextSpan>[
+                  TextSpan(text: 'Esqueceu seus dados de login?'),
+                  TextSpan(
+                      text: ' Obtenha ajuda para entrar.',
+                      style: TextStyle(color: Colors.blue[900], fontWeight: FontWeight.w700,),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          print('CLICOU');
+                        }),                  
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
