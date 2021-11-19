c206bbc079e408034c2711c81a402a9f3be969ee
Ana Clara Fagundes <79975018+fagundesana017@users.noreply.github.com>
2021-11-19 14:22:11 -0300

    appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,48 +1,102 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:insta_clone/main.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: getAppBar(),
+      // backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
       bottomNavigationBar: footer(),
     );
   }
 
-  Widget footer() {
-     List bottomItems = [
-       "assets/images/home_active_icon.svg",
-       "assets/images/search_active_icon.svg",
-       "assets/images/upload_active_icon.svg",
-       "assets/images/love_active_icon.svg",
-       "assets/images/account_active_icon.svg",
-     ];
+  getAppBar() {
+    if (pageIndex == 0) {
+      return AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            SvgPicture.asset(
+              "assets/images/camera_icon.svg",
+              width: 30.0,
+            ),
+            Text(
+              "Instagram",
+              style: TextStyle(fontFamily: "Billabong", fontSize: 35),
+            ),
+            SvgPicture.asset(
+              "assets/images/message_icon.svg",
+              width: 30,
+            )
+          ],
+        ),
+      );
+    } else if (pageIndex == 1) {
+      return null;
+    } else if (pageIndex == 2) {
+      return AppBar(backgroundColor: Colors.black);
+    }
+  }
 
-     return Container(
-       width: double.infinity,
-       height: 80.0,
-       decoration: BoxDecoration(
-          color: Colors.black,
-       ),
-       child: Padding(
-         padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 20.0),
-         child: Row(
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-           children: List.generate(bottomItems.length, (index) {
-             return SvgPicture.asset(bottomItems[index], width: 27.0,);
-           }),
-         ),
-       ),
-     );
+  Widget footer() {
+    List bottomItems = [
+      pageIndex == 0
+          ? "assets/images/home_active_icon.svg"
+          : "assets/images/home_icon.svg ",
+      pageIndex == 1
+          ? "assets/images/search_active_icon.svg"
+          : "assets/images/search_icon.svg",
+      pageIndex == 2
+          ? "assets/images/upload_active_icon.svg"
+          : "assets/images/upload_icon.svg",
+      pageIndex == 3
+          ? "assets/images/love_active_icon.svg"
+          : "assets/images/love_icon.svg",
+      pageIndex == 4
+          ? "assets/images/account_active_icon.svg"
+          : "assets/images/account_icon.svg",
+    ];
+
+    return Container(
+      width: double.infinity,
+      height: 80.0,
+      decoration: BoxDecoration(
+        color: Colors.black,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(bottomItems.length, (index) {
+            return InkWell(
+                onTap: () {
+                  selectedTab(index);
+                },
+                child: SvgPicture.asset(
+                  bottomItems[index],
+                  width: 27.0,
+                ));
+          }),
+        ),
+      ),
+    );
+  }
+
+  selectedTab(index) {
+    pageIndex = index;
   }
 }
