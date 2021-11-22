31751854bc15e81eb9f256e292ea70f1bfd7c274
Ítalo Covas <italocovas@gmail.com>
2021-11-22 15:54:24 -0300

    Desfazendo alterações

--- lib/pages/newpost_page.dart
+++ lib/pages/newpost_page.dart
@@ -1,15 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:camera/camera.dart';
-
-List<CameraDescription>? cameras;
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  cameras = await availableCameras();
-  runApp(const NewPost());
-}
 
 class NewPost extends StatefulWidget {
   const NewPost({ Key? key }) : super(key: key);
@@ -19,33 +8,10 @@ class NewPost extends StatefulWidget {
 }
 
 class _NewPostState extends State<NewPost> {
-  CameraController? controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = CameraController(cameras![0], ResolutionPreset.max);
-    controller!.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
-  }
-  
-   @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (!controller!.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: CameraPreview(controller!),
+    return Container(
+
     );
   }
 }
