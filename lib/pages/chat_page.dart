e6fa081d43052adcb72cb9cd412fb2d264b21263
emattos <emattos@lideraviacao.com.br>
2021-11-25 14:03:28 -0300

    Correção tabs chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -24,11 +24,13 @@ class _ChatPageState extends State<ChatPage> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Text('framework.digital',
-                    style: TextStyle(
-                      color: white,
-                      fontSize: 16,
-                    )),
+                Text(
+                  'framework.digital',
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 16,
+                  ),
+                ),
                 Icon(Icons.keyboard_arrow_down, size: 19),
               ],
             ),
@@ -65,57 +67,27 @@ class _ChatPageState extends State<ChatPage> {
                 ],
               ),
               Expanded(
-                child: SingleChildScrollView(
-                  child: TabBarView(
-                    children: <Widget>[
-                      Padding(
-                    padding: EdgeInsets.only(top: 15),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          width: size.width - 30,
-                          height: 30,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: textFieldBackground,
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              prefixIcon: Icon(Icons.search,
-                                  color: white.withOpacity(0.3)),
-                              hintText: 'Pesquisar',
-                              contentPadding: EdgeInsets.only(bottom: 14),
-                              hintStyle: TextStyle(
-                                  color: white.withOpacity(0.3),
-                                  fontWeight: FontWeight.w500),
-                            ),
-                            style: const TextStyle(
-                              color: white,
-                            ),
-                            cursorColor: white.withOpacity(0.3),
-                          ),
-                        ),
-                        SizedBox(height: 15),
-                        Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Column(
-                              children: List.generate(stories.length, (index) {
-                                return ChatItem(
-                                  img: stories[index]['img'],
-                                  name: stories[index]['name'],
-                                );
-                              })
-                            ),
-                          ),
-                      ],
+                child: TabBarView(
+                  children: [
+                    SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15),
+                        child: Column(
+                            children: List.generate(
+                          stories.length,
+                          (index) {
+                            return ChatItem(
+                              img: stories[index]['img'],
+                              name: stories[index]['name'],
+                            );
+                          },
+                        )),
+                      ),
                     ),
-                  ),
-                  Text('oi'),
-                    ]
-                  ),
+                    Container(color: Colors.blue),
+                  ],
                 ),
-              ),
+              )
             ],
           ),
         ),
@@ -123,4 +95,3 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 }
-
